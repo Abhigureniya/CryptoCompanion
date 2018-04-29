3493332dfcd68edc2e0cddab3b91af49f9581943
Abhijeet Singh Gureniya <abhigureniya1@gmail.com>
2018-04-29 17:50:34 +0530

    Bitcoin and Ethereum Data Success. Reload Left.

--- app/src/main/java/com/ants/cc/cryptocompanion/CurrencyRatesActivity.java
+++ app/src/main/java/com/ants/cc/cryptocompanion/CurrencyRatesActivity.java
@@ -9,10 +9,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -31,6 +36,7 @@ public class CurrencyRatesActivity extends AppCompatActivity {
             "https://www.buyucoin.com/api/v1.2/currency/btc"
 
     };
+    String[] bitcoinAPISNames = {"ZebPay","CoinDelta","Koinex","BuyUCoin"};
     String[] ethereumAPIs = {
             "https://www.zebapi.com/api/v1/market/ticker-new/eth/inr",
             "https://coindelta.com/api/v1/public/getticker/",
@@ -39,9 +45,11 @@ public class CurrencyRatesActivity extends AppCompatActivity {
 
     };
 
+    ProgressBar progressBar2;
+
     String commodity;
 
-    TableLayout backupLayout;
+    TableLayout tableLayout;
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
@@ -49,6 +57,8 @@ public class CurrencyRatesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency_rates);
         Bundle extras = getIntent().getExtras();
+        progressBar2 = (ProgressBar)findViewById(R.id.progressBar2);
+        progressBar2.setVisibility(View.VISIBLE);
         if(extras != null){
             commodity = extras.getString("Commodity");
             String currency = extras.getString("Currency");
@@ -56,45 +66,32 @@ public class CurrencyRatesActivity extends AppCompatActivity {
             TextView t = (TextView)findViewById(R.id.textView);
             t.setText("Rate Card");
         }
-        TableLayout tableLayout = (TableLayout)findViewById(R.id.tableLayout);
-        backupLayout = tableLayout;
-        /*ContentDownloader cd = new ContentDownloader();
-        ContentDownloader cd1 = new ContentDownloader();
-        cd.execute(bitcoinAPIs[0],bitcoinAPIs[1],bitcoinAPIs[2],bitcoinAPIs[3]);
-        cd1.execute(ethereumAPIs[0],ethereumAPIs[1],ethereumAPIs[2],ethereumAPIs[3]);*/
-        for(int i=1; i<=10; i++){
-            TableRow tr1 = new TableRow(this);
-            tr1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        tableLayout = (TableLayout)findViewById(R.id.tableLayout);
+        ContentDownloader cd = new ContentDownloader();
 
-            TextView t1 = new TextView(this);
-            TextView t2 = new TextView(this);
-            TextView t3 = new TextView(this);
-            t1.setText("Test Data");
-            t1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            t2.setText("Test Data");
-            t2.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            t3.setText("Test Data");
-            t3.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            t1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-            t2.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-            t3.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-            tr1.addView(t1);
-            tr1.addView(t2);
-            tr1.addView(t3);
-            tableLayout.addView(tr1, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+        if(commodity.equals("Bitcoin")){
+            cd.execute(bitcoinAPIs[0],bitcoinAPIs[1],bitcoinAPIs[2],bitcoinAPIs[3]);
+        }else if(commodity.equals("Ethereum")){
+            cd.execute(ethereumAPIs[0],ethereumAPIs[1],ethereumAPIs[2],ethereumAPIs[3]);
         }
+
+
+
+
 
     }
 
 
     class ContentDownloader extends AsyncTask<String, Void, TableRow[]>{
 
-        TableRow[] results;
+        TableRow[] results = new TableRow[4];
 
+        @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
         @Override
         protected TableRow[] doInBackground(String... urls) {
             ArrayList<String> al = new ArrayList<>();
-            for(String s : urls){
+            for(int i=0; i<=3; i++){
+                String s = urls[i];
                 try {
                     URL url = new URL(s);
                     HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
@@ -115,15 +112,89 @@ public class CurrencyRatesActivity extends AppCompatActivity {
                 }
             }
 
+                for(int i=0; i<=3; i++){
+                    TableRow tr1 = new TableRow(getApplicationContext());
+                    tr1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+
+                    TextView t1 = new TextView(getApplicationContext());
+                    TextView t2 = new TextView(getApplicationContext());
+                    TextView t3 = new TextView(getApplicationContext());
+                    t1.setText(bitcoinAPISNames[i]);
+                    t1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                    t2.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+                    if(i==0){
+                        try {
+                            JSONObject jo = new JSONObject(al.get(i));
+                            t2.setText(jo.getString("buy"));
+                            t3.setText(jo.getString("sell"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }else if(i==1){
+                        try {
+                            JSONArray ja = new JSONArray(al.get(i));
+                            JSONObject jo;
+                            if(commodity.equals("Bitcoin")){
+                                jo = ja.getJSONObject(0);
+                            }else{
+                                jo = ja.getJSONObject(1);
+                            }
+                            t2.setText(jo.getString("Ask"));
+                            t3.setText(jo.getString("Bid"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }else if(i==2){
+                        try {
+                            JSONObject jo = new JSONObject(al.get(i));
+                            JSONObject stats = jo.getJSONObject("stats");
+                            JSONObject inr = stats.getJSONObject("inr");
+                            JSONObject btc;
+                            if(commodity.equals("Bitcoin")){
+                                btc = inr.getJSONObject("BTC");
+                            }else{
+                                btc = inr.getJSONObject("ETH");
+                            }
+                            t2.setText(btc.getString("lowest_ask"));
+                            t3.setText(btc.getString("highest_bid"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }else if(i==3){
+                        try {
+                            JSONObject jo = new JSONObject(al.get(i));
+                            JSONObject data = jo.getJSONObject("data");
+                            t2.setText(data.getString("ask"));
+                            t3.setText(data.getString("bid"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    t3.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                    t1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+                    t2.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+                    t3.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+                    tr1.addView(t1);
+                    tr1.addView(t2);
+                    tr1.addView(t3);
+                    results[i]=tr1;
+                    //tableLayout.addView(tr1, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+                }
+
+
             return results;
         }
 
         @Override
         protected void onPostExecute(TableRow[] tableRows) {
-            /*for(TableRow t : tableRows){
-                System.out.println(t);
-            }*/
+            for(TableRow t : tableRows){
+                //System.out.println(t);
+                tableLayout.addView(t, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+            }
             System.out.println("Execution Complete");
+            progressBar2.setVisibility(View.INVISIBLE);
         }
     }
 
